106e7e6d79e3a399035c0291bbd3592756e7662b
nagaraju <nagaraju@kyatiservices>
2019-04-19 19:39:40 +0530

    Testcase

---
+++ Hema_GIT_Project/src/Sample_Test_Case.java
@@ -0,0 +1,11 @@
+
+public class Sample_Test_Case {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my selenium Training");
+		System.out.println("This is my software Training");
+
+	}
+
+}
