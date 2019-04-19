b7ab609a71bf6c781c027fc6d6d176f92290839d
nagaraju <nagaraju@kyatiservices>
2019-04-19 19:46:12 +0530

    testcase

--- Hema_GIT_Project/src/Sample_Test_Case.java
+++ Hema_GIT_Project/src/Sample_Test_Case.java
@@ -3,9 +3,15 @@ public class Sample_Test_Case {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This is my selenium Training");
-		System.out.println("This is my software Training");
-
+		String str1 = "This is my training";
+		String str2 = "This is my TRAINING";
+		if(str1.equalsIgnoreCase(str2)){
+			System.out.println("Matching values");
+		}
+		else
+		{
+			System.out.println("Does not matching");
+		}
 	}
 
 }
